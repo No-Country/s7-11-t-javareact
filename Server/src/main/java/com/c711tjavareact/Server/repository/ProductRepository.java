298e80499912989e4d56cd261ea21d27756cdf06
Alan Cejas <cejasalandavid01@gmail.com>
2023-03-30 17:49:58 -0300

    Update(Backend): Add Product Category and Requirement Repository

---
+++ Server/src/main/java/com/c711tjavareact/Server/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.c711tjavareact.Server.repository;
+
+import com.c711tjavareact.Server.model.entity.Product;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+  Optional<Product> findByName(String name);
+}
