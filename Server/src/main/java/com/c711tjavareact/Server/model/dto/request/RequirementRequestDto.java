8b93cf3c7d1393eddee555884ce83e8d7fe8867d
Alan Cejas <cejasalandavid01@gmail.com>
2023-03-30 16:09:28 -0300

    Update(Backend): Add Product Category and Requirement Dtos

---
+++ Server/src/main/java/com/c711tjavareact/Server/model/dto/request/RequirementRequestDto.java
@@ -0,0 +1,21 @@
+package com.c711tjavareact.Server.model.dto.request;
+
+import com.c711tjavareact.Server.model.entity.Product;
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RequirementRequestDto {
+  @Id
+  private Long id;
+
+  @NotBlank(message = "Not Empty")
+  private String name;
+
+  private LocalDateTime updateDate;
+
+  private List<Product> productList;
+}
