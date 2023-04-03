d707d2560122cfaba3889f861757dffb098b532e
lokywolf2295 <matiasesek1995@gmail.com>
2023-04-03 15:47:43 -0300

    Update(Backend): update productMapper mapper

--- Server/src/main/java/com/c711tjavareact/Server/model/dto/response/ProductResponseDto.java
+++ Server/src/main/java/com/c711tjavareact/Server/model/dto/response/ProductResponseDto.java
@@ -5,20 +5,25 @@ import com.c711tjavareact.Server.model.entity.Requirement;
 import java.io.Serializable;
 import java.util.List;
 import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * A DTO for the {@link com.c711tjavareact.Server.model.entity.Product} entity
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductResponseDto implements Serializable {
-  private final Long id;
+  private Long id;
   @NotBlank(message = "Not Empty")
-  private final String name;
-  private final float price;
-  private final int quantity;
-  private final int discount;
-  private final boolean status;
-  private final Category category;
-  private final List<Requirement> requirementList;
+  private String name;
+  private float price;
+  private int quantity;
+  private int discount;
+  private boolean status;
+  private Category category;
+  private List<Requirement> requirementList;
 }
--- Server/src/main/java/com/c711tjavareact/Server/model/mapper/ProductMapper.java
+++ Server/src/main/java/com/c711tjavareact/Server/model/mapper/ProductMapper.java
@@ -1,6 +1,7 @@
 package com.c711tjavareact.Server.model.mapper;
 
 import com.c711tjavareact.Server.model.dto.request.ProductRequestDto;
+import com.c711tjavareact.Server.model.dto.response.ProductResponseDto;
 import com.c711tjavareact.Server.model.entity.Product;
 
 public class ProductMapper {
@@ -15,4 +16,27 @@ public class ProductMapper {
 
         return product;
     }
+
+    public ProductResponseDto dtoToEntity(Product entity) {
+
+        ProductResponseDto responseDto = new ProductResponseDto();
+
+        responseDto.setName(entity.getName());
+        responseDto.setPrice(entity.getPrice());
+        responseDto.setDiscount(entity.getDiscount());
+
+        return  responseDto;
+    }
+
+    public Product updateProduct(Product entity, ProductRequestDto requestDto){
+
+        Product product = new Product();
+
+        product.setName(requestDto.getName());
+        product.setPrice(requestDto.getPrice());
+        product.setDiscount(requestDto.getDiscount());
+
+        return product;
+    }
+
 }
