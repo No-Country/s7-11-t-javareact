1e98376ea598b65fbad2b584bf62e6c567dcefd9
lucerocamila <lucerogonzalez1997@gmail.com>
2023-04-03 23:57:20 -0300

    feat(src/):update components

--- client/src/pages/Register/index.jsx
+++ client/src/pages/Register/index.jsx
@@ -1,79 +1,79 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-      <div
-        className="grid  min-h-screen bg-cover bg-center bg-fixed"
-        style={{ backgroundColor: `white` }}
-      >
-        <div className="grid justify-items-center items-center h-full">
-          <div className="grid justify-items-center w-full max-w-md p-8 rounded-lg shadow-lg border border-gray-200 bg-gray-100">
-            <h1 className="text-2xl font-bold mb-4">Registro</h1>
-            <form className="space-y-4">
-              <div>
-                <label
-                  className="block text-gray-700 font-bold mb-2"
-                  htmlFor="username"
-                >
-                  Nombre de usuario
-                </label>
-                <input
-                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="username"
-                  type="text"
-                  placeholder="Nombre de usuario"
-                />
-              </div>
-              <div>
-                <label
-                  className="block text-gray-700 font-bold mb-2"
-                  htmlFor="email"
-                >
-                  Correo electrónico
-                </label>
-                <input
-                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="email"
-                  type="email"
-                  placeholder="Correo electrónico"
-                />
-              </div>
-              <div>
-                <label
-                  className="block text-gray-700 font-bold mb-2"
-                  htmlFor="password"
-                >
-                  Contraseña
-                </label>
-                <input
-                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="password"
-                  type="password"
-                  placeholder="Contraseña"
-                />
-              </div>
-              <div>
-                <label
-                  className="block text-gray-700 font-bold mb-2"
-                  htmlFor="confirm-password"
-                >
-                  Confirmar contraseña
-                </label>
-                <input
-                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="confirm-password"
-                  type="password"
-                  placeholder="Confirmar contraseña"
-                />
-              </div>
-              <div className="grid justify-items-center">
-                <button
-                  className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="button"
-                >
-                  Registrarse
-                </button>
-                <p className="mt-4">
+  return (
+    <div
+      className="grid  min-h-screen bg-cover bg-center bg-fixed"
+      style={{ backgroundColor: `white` }}
+    >
+      <div className="grid justify-items-center items-center h-full p-4 ">
+        <div className="grid justify-items-center w-full max-w-md px-[4%] min-w-[321px]:px-8 py-8 rounded-lg shadow-lg border border-gray-200 bg-gray-100">
+          <h1 className="text-2xl font-bold mb-4">Registro</h1>
+          <form className="space-y-4 w-full px-[10%]">
+            <div>
+              <label
+                className="block text-gray-700 font-bold mb-2"
+                htmlFor="username"
+              >
+                Nombre de usuario
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Nombre de usuario"
+              />
+            </div>
+            <div>
+              <label
+                className="block text-gray-700 font-bold mb-2"
+                htmlFor="email"
+              >
+                Correo electrónico
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Correo electrónico"
+              />
+            </div>
+            <div>
+              <label
+                className="block text-gray-700 font-bold mb-2"
+                htmlFor="password"
+              >
+                Contraseña
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Contraseña"
+              />
+            </div>
+            <div>
+              <label
+                className="block text-gray-700 font-bold mb-2"
+                htmlFor="confirm-password"
+              >
+                Confirmar contraseña
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="confirm-password"
+                type="password"
+                placeholder="Confirmar contraseña"
+              />
+            </div>
+            <div className="grid justify-items-center">
+              <button
+                className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Registrarse
+              </button>
+              <div className="flex flex-wrap mt-[4%] text-center">
                 ¿Ya estás registrado?
                 <Link
                   className="text-blue-500 hover:text-blue-700 ml-1"
@@ -81,14 +81,13 @@ const Register = () => {
                 >
                   Iniciar sesión
                 </Link>
-              </p>
               </div>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </div>
-    );
-  };
-  
-  export default Register;
-  
+    </div>
+  );
+};
+
+export default Register;
