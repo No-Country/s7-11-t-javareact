0a73dfecfaf6d212ab4ed38cf8271969772d9663
Lucero Camila Gonzalez <108992998+lucerocamila@users.noreply.github.com>
2023-04-05 01:26:06 -0300

    Revert "Carlos/modify list page responsive view"

--- client/src/pages/Register/index.jsx
+++ client/src/pages/Register/index.jsx
@@ -1,22 +1,15 @@
-import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  
-  const onSubmit = data => {
-    console.log(data);
-  };
-  
   return (
     <div
-      className="grid  min-h-screen bg-cover bg-center bg-fixed"
+      className="grid  min-h-screen bg-cover bg-center bg-fixed bg-gradient"
       style={{ backgroundColor: `white` }}
     >
-      <div className="grid justify-items-center items-center h-full">
-        <div className="grid justify-items-center w-full max-w-md p-8 rounded-lg shadow-lg border border-gray-200 bg-gray-100">
+      <div className="grid justify-items-center items-center h-full p-4 ">
+        <div className="grid justify-items-center w-full max-w-md px-[4%] min-w-[321px]:px-8 py-8 rounded-lg shadow-lg border border-gray-200 bg-gray-100">
           <h1 className="text-2xl font-bold mb-4">Registro</h1>
-          <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          <form className="space-y-4 w-full px-[10%]">
             <div>
               <label
                 className="block text-gray-700 font-bold mb-2"
@@ -25,13 +18,11 @@ const Register = () => {
                 Nombre de usuario
               </label>
               <input
-                {...register("username", { required: true })}
-                className={`appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.username ? 'border-red-500' : ''}`}
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="username"
                 type="text"
                 placeholder="Nombre de usuario"
               />
-              {errors.username && <p className="text-red-500">Este campo es requerido</p>}
             </div>
             <div>
               <label
@@ -41,14 +32,11 @@ const Register = () => {
                 Correo electrónico
               </label>
               <input
-                {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-                className={`appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.email ? 'border-red-500' : ''}`}
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="email"
                 type="email"
                 placeholder="Correo electrónico"
               />
-              {errors.email && errors.email.type === 'required' && <p className="text-red-500">Este campo es requerido</p>}
-              {errors.email && errors.email.type === 'pattern' && <p className="text-red-500">Correo electrónico inválido</p>}
             </div>
             <div>
               <label
@@ -58,14 +46,11 @@ const Register = () => {
                 Contraseña
               </label>
               <input
-                {...register("password", { required: true, minLength: 6 })}
-                className={`appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.password ? 'border-red-500' : ''}`}
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="password"
                 type="password"
                 placeholder="Contraseña"
               />
-              {errors.password && errors.password.type === 'required' && <p className="text-red-500">Este campo es requerido</p>}
-              {errors.password && errors.password.type === 'minLength' && <p className="text-red-500">La contraseña debe tener al menos 6 caracteres</p>}
             </div>
             <div>
               <label
@@ -75,23 +60,20 @@ const Register = () => {
                 Confirmar contraseña
               </label>
               <input
-                {...register("confirm-password", { required: true, minLength: 6 })}
-                className={`appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.password ? 'border-red-500' : ''}`}
-                id="password"
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="confirm-password"
                 type="password"
                 placeholder="Confirmar contraseña"
               />
-              {errors.password && errors.password.type === 'required' && <p className="text-red-500">Este campo es requerido</p>}
-              {errors.password && errors.password.type === 'minLength' && <p className="text-red-500">La contraseña debe tener al menos 6 caracteres</p>}
             </div>
-              <div className="grid justify-items-center">
-                <button
-                  className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="button"
-                >
-                  Registrarse
-                </button>
-                <p className="mt-4">
+            <div className="grid justify-items-center">
+              <button
+                className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Registrarse
+              </button>
+              <div className="flex flex-wrap mt-[4%] text-center">
                 ¿Ya estás registrado?
                 <Link
                   className="text-blue-500 hover:text-blue-700 ml-1"
@@ -99,14 +81,13 @@ const Register = () => {
                 >
                   Iniciar sesión
                 </Link>
-              </p>
               </div>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </div>
-    );
-  };
-  
-  export default Register;
-  
+    </div>
+  );
+};
+
+export default Register;
