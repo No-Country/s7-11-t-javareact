b43a48f897d7b4c9caaf4806c1a7044c2a83d8b2
lokywolf2295 <matiasesek1995@gmail.com>
2023-04-06 19:05:16 -0300

    Update(Backend): add deleteProduct method in IProductService

--- Server/src/main/java/com/c711tjavareact/Server/service/IProductService.java
+++ Server/src/main/java/com/c711tjavareact/Server/service/IProductService.java
@@ -1,8 +1,10 @@
 package com.c711tjavareact.Server.service;
 
 import com.c711tjavareact.Server.model.dto.request.ProductRequestDto;
+import com.c711tjavareact.Server.model.entity.Product;
 import org.springframework.http.ResponseEntity;
 
 public interface IProductService {
 	ResponseEntity<ProductRequestDto>createProduct(ProductRequestDto productRequestDto);
+	void deleteProduct(Long id);
 }
