166b4528c87ae5b3a0dfd94d0e0a3c8c5259310c
marcos <marcosep192000@hotmail.com>
2023-04-08 11:49:34 -0300

    Update(Backend):fix CreateRoles class to check if they exist

--- Server/src/main/java/com/c711tjavareact/Server/security/util/CreateRoles.java
+++ Server/src/main/java/com/c711tjavareact/Server/security/util/CreateRoles.java
@@ -2,25 +2,36 @@ package com.c711tjavareact.Server.security.util;
 
 import com.c711tjavareact.Server.security.entity.Rol;
 import com.c711tjavareact.Server.security.enums.RolNombre;
+import com.c711tjavareact.Server.security.repository.RolRepository;
 import com.c711tjavareact.Server.security.service.RolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import javax.management.relation.Role;
+import java.util.List;
 
 @Component
 public class CreateRoles implements CommandLineRunner {
 
 	@Autowired
 	RolService rolService;
-
+    @Autowired
+	RolRepository rolRepository;
 	@Override
 	public void run(String... args) throws Exception {
 
-		Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
-		Rol rolUser = new Rol(RolNombre.ROLE_USER);
-		rolService.save(rolAdmin);
-		rolService.save(rolUser);
+		List<Rol> rol = rolRepository.findAll();
+		if (rol.isEmpty())
+		{
+			Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
+			Rol rolUser = new Rol(RolNombre.ROLE_USER);
+			rolService.save(rolAdmin);
+			rolService.save(rolUser);
+		}
+
 
 
 	}
 }
+
