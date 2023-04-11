c759c64640d514e16d1daf10f73ed77e6a823f8a
lucerocamila <lucerogonzalez1997@gmail.com>
2023-04-11 19:40:47 -0300

    fix(layouts):GoBack BTN

--- client/src/layouts/GoBack/index.jsx
+++ client/src/layouts/GoBack/index.jsx
@@ -1,3 +1,4 @@
+
 import { IoChevronBackOutline } from "react-icons/io5"
 import { useNavigate } from "react-router"
 
@@ -9,13 +10,13 @@ const GoBack = ({ goTo = -1 }) => {
   }
 
   return (
-    <header className="sticky top-0 z-20 flex flex-row-reverse w-full justify-between items-center m-auto">
-      <span className="w-full flex justify-center bg-white py-4 mr-11 text-2xl">
+    <header className="fixed pt-1.5 top-5 left-0 flex flex-row-reverse justify-between items-center m-auto">
+      <span className="w-full flex justify-center py-4 mr-11 text-2xl">
         
       </span>
 
       <button
-        className="text-lg  bg-white px-1.5 py-1.5 border-2 border-neutral-200 rounded-xl  focus:border-blue-700 ml-4"
+        className="text-lg px-1.5 py-1.5 border-2 border-neutral-200 rounded-xl focus:border-blue-700 ml-4 min-w-max flex-shrink-0"
         onClick={handleGoBack}
       >
         <IoChevronBackOutline />
