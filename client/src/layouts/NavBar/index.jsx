ff7ad7014b28f54eb094ff84cc1f1ee53e4673fb
lucerocamila <lucerogonzalez1997@gmail.com>
2023-04-11 19:35:15 -0300

    feat(layouts):upadate NavBar

--- client/src/layouts/NavBar/index.jsx
+++ client/src/layouts/NavBar/index.jsx
@@ -1,63 +1,87 @@
-import React from 'react';
+import smallLogo from "../../assets/images/smallLogo.png";
+import { useState } from "react";
 
-const NavBar = () => {
-    return (
-        <div className='w-full'>
-           <nav className="flex flex-wrap items-center justify-between px-2 py-3 bg-white">
-  <div className="flex items-center mr-4">
-    <img
-      className="h-8 w-auto sm:h-10"
-      src="https://res.cloudinary.com/dw639wmis/image/upload/v1680807861/selected_1_1_x1ocsl.png"
-      alt="Workflow"
-    />
-    <span className="text-xl font-bold text-black ml-2">Compra LISTo</span>
-  </div>
-  <div className="block lg:hidden">
-    <button className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white">
-      <svg
-        className="h-3 w-3 fill-current"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <title>Menu</title>
-        <path d="M0 3a3 3 0 013-3h14a3 3 0 110 6H3a3 3 0 01-3-3zM0 10a3 3 0 013-3h14a3 3 0 110 6H3a3 3 0 01-3-3zM3 17a3 3 0 100 6h14a3 3 0 100-6H3z" />
-      </svg>
-    </button>
-  </div>
-  <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 lg:justify-end">
-    <div className="text-sm lg:flex-grow">
-      <a
-        href="#responsive-header"
-        className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-gray-400 mr-4"
-      >
-        Home
-      </a>
-      <a
-        href="#responsive-header"
-        className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-gray-400 mr-4"
-      >
-        My Lists
-      </a>
-      <a
-        href="#responsive-header"
-        className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-gray-400"
-      >
-        About
-      </a>
-    </div>
-    <div>
-      <a
-        href="#"
-        className="inline-block text-sm px-4 py-2 leading-none border rounded text-black border-white hover:border-transparent hover:text-gray-500 hover:bg-white mt-4 lg:mt-0"
-      >
-        Sign Up/Out
-      </a>
-    </div>
-  </div>
-</nav>
- 
+function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="bg-transparent">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8"> 
+        <div className="relative flex items-center justify-center h-16">
+           <div className="flex ml-[45%] lg:ml-3">
+            <img className="h-8 w-auto sm:h-10" src={smallLogo} alt="Logo" />
+            <span className="hidden lg:block text-xl font-bold text-black font-poppins ml-3 mr-2 pr-3">
+              Compra LISTo
+            </span>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+           
+            <button
+              className={`flex flex-col w-5 h-5 border-0 bg-transparent gap-1 btn-animation ${
+                isOpen ? "open" : ""
+              }`}
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <span className="w-full bg-black h-[2px] rounded-md transition-all duration-500 origin-left btn-line"></span>
+              <span className="w-full bg-black h-[2px] rounded-md transition-all duration-500 origin-left btn-line"></span>
+              <span className="w-full bg-black h-[2px] rounded-md transition-all duration-500 origin-left btn-line"></span>
+            </button>
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                <a
+                  href="/"
+                  className="text-black  hover:bg-[#DAEDFF] px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Home
+                </a>
+                <a
+                  href="/"
+                  className="text-black hover:bg-[#DAEDFF] px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  About
+                </a>
+                <a
+                  href="/"
+                  className="text-black hover:bg-[#DAEDFF] px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Contact
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-    );
-};
+
+        <div
+          className={`${
+            isOpen ? "block" : "hidden"
+          } lg:hidden absolute bg-white w-full left-0 `}
+        >
+          <div className="px-2 pt-5 pb-3 space-y-1">
+            <a
+              href="/home"
+              className="text-black hover:bg-[#DAEDFF] block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Home
+            </a>
+            <a
+              href="/"
+              className="text-black hover:bg-[#DAEDFF] block px-3 py-2 rounded-md text-base font-medium"
+            >
+              About
+            </a>
+            <a
+              href="/"
+              className="text-black hover:bg-[#DAEDFF] block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 export default NavBar;
