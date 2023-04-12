bb4cfb5aaa0fc238ef05fb361b49bbde77915dd2
Ana Juárez <lisa13suarez@gmail.com>
2023-04-12 11:00:02 -0600

    Styles(src/compontents/Card): Add height in card

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -4,7 +4,7 @@ const Card = ({image, service, paragraph }) => {
   return (
     <div className="flex flex-col max-w-[22rem] lg:w-full lg:h-full">
       <div
-        className={`flex flex-col justify-center p-[2%] max-270px]:h-[9.5rem] lg:h-full lg:w-full lg:bg-transparent bg-gray-50 drop-shadow-md rounded-2xl text-center text-neutral-900`}
+        className={`flex flex-col justify-center p-[2%] max-270px]:h-[9.5rem] lg:h-full lg:w-full lg:bg-transparent bg-gray-50 drop-shadow-md rounded-2xl text-center text-neutral-900 h-[15rem]`}
       >
         <div className="pl-[3%] justify-self-center self-center  h-[7rem] max-[270px]:h-[6rem] flex-initial max-w-sm rounded-full flex-shrink-0">
           <img
