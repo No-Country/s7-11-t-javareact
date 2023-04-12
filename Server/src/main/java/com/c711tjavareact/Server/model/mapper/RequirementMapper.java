6106174d5df5ef571856f6adcb349044e8ccb6a4
lokywolf2295 <matiasesek1995@gmail.com>
2023-04-12 17:23:55 -0300

    Update(Backend): add CRUD methods to RequirementMapper class

--- Server/src/main/java/com/c711tjavareact/Server/model/mapper/RequirementMapper.java
+++ Server/src/main/java/com/c711tjavareact/Server/model/mapper/RequirementMapper.java
@@ -1,6 +1,50 @@
 package com.c711tjavareact.Server.model.mapper;
 
+
+import com.c711tjavareact.Server.model.dto.request.RequirementRequestDto;
+import com.c711tjavareact.Server.model.dto.response.RequirementResponseDto;
+import com.c711tjavareact.Server.model.entity.Requirement;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RequirementMapper {
 
-    //TODO: 1830/03/2023 Create RequirementMapper
+    public Requirement entityToDto(RequirementRequestDto Request) {
+        Requirement requirement = new Requirement();
+
+        requirement.setName(Request.getName());
+        requirement.setProductList(Request.getProductList());
+
+        return requirement;
+    }
+
+    public RequirementResponseDto dtoToEntity(Requirement entity) {
+        RequirementResponseDto requirementResponseDto = new RequirementResponseDto();
+
+        requirementResponseDto.setId(entity.getId());
+        requirementResponseDto.setName(entity.getName());
+
+/*    List<ProductResponseDto> listProductDto = new ArrayList<>();
+    entity.getProducts().forEach(product -> {
+      ProductResponseDto productResponseDto = productMapper.dtoToEntity(product);
+      listProductDto.add(productResponseDto);
+    });
+
+    categoryResponseDto.setProducts(listProductDto);*/
+
+        return requirementResponseDto;
+    }
+
+    public Requirement updateRequirement(Requirement requirement, RequirementRequestDto requestDto){
+
+        requirement.setName(requestDto.getName());
+        requirement.setProductList(requestDto.getProductList());
+
+        return requirement;
+
+    }
+    public Requirement updateSoftDelete(Requirement requirement, boolean status){
+        requirement.setStatus(status);
+        return requirement;
+    }
 }
