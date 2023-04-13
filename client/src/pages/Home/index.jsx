a755361ab1903adc8860c30b39a6fd05d9524fd1
Ana Juárez <lisa13suarez@gmail.com>
2023-04-12 22:31:38 -0600

    Styles (src/pages/landingPage): Add backticks

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -16,34 +16,36 @@ const Home = () => {
   ];
 
   return (
-    <div className="container mx-auto p-4 grid grid-cols-1 gap-4 h-full md:grid-cols-2 lg:grid-cols-3">
+    <>
       <Navbar/>
       <GoBack />
-      <div className="col-span-full flex justify-between items-center h-full mb-6">
-        <h1 className="text-2xl font-bold">Listas recientes</h1>
+      <div className="container mx-auto p-4 grid grid-cols-1 gap-4 h-full md:grid-cols-2 lg:grid-cols-3">
+        <div className="col-span-full flex justify-between items-center h-full mb-6">
+          <h1 className="text-2xl font-bold">Listas recientes</h1>
+          <Link to="/list">
+            <div className="w-16 h-16 lg:hidden bg-gray-200 rounded-full flex justify-center items-center cursor-pointer hover:bg-gray-300">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus">
+                <line x1="12" y1="5" x2="12" y2="19"></line>
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+              </svg>
+            </div>
+          </Link>
+        </div>
+        {recentLists.map((list) => (
+      
+          <ListsCard key={list.id} title={list.name} subtitle={`Actualizada ${list.date}`} />
+        ))}
         <Link to="/list">
-          <div className="w-16 h-16 lg:hidden bg-gray-200 rounded-full flex justify-center items-center cursor-pointer hover:bg-gray-300">
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus">
-              <line x1="12" y1="5" x2="12" y2="19"></line>
-              <line x1="5" y1="12" x2="19" y2="12"></line>
-            </svg>
-          </div>
-        </Link>
+          <div className="hidden lg:block ">
+    <div className="max-w-[22rem] h-28 bg-gray-200 rounded-lg flex justify-center items-center cursor-pointer hover:bg-gray-300">
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus">
+        <line x1="12" y1="5" x2="12" y2="19"></line>
+        <line x1="5" y1="12" x2="19" y2="12"></line>
+      </svg>
+    </div></div>
+  </Link>
       </div>
-      {recentLists.map((list) => (
-    
-        <ListsCard key={list.id} title={list.name} subtitle={`Actualizada ${list.date}`} />
-      ))}
-       <Link to="/list">
-        <div className="hidden lg:block ">
-  <div className="max-w-[22rem] h-28 bg-gray-200 rounded-lg flex justify-center items-center cursor-pointer hover:bg-gray-300">
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus">
-      <line x1="12" y1="5" x2="12" y2="19"></line>
-      <line x1="5" y1="12" x2="19" y2="12"></line>
-    </svg>
-  </div></div>
-</Link>
-    </div>
+    </>
   );
 };
 
