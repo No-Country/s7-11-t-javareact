4153abf43a5a20bd92112036ff11353ec35bd930
lucerocamila <lucerogonzalez1997@gmail.com>
2023-04-13 15:01:40 -0300

    style(GoBack):remove padding top, run prettier

--- client/src/layouts/GoBack/index.jsx
+++ client/src/layouts/GoBack/index.jsx
@@ -1,19 +1,16 @@
-
-import { IoChevronBackOutline } from "react-icons/io5"
-import { useNavigate } from "react-router"
+import { IoChevronBackOutline } from "react-icons/io5";
+import { useNavigate } from "react-router";
 
 const GoBack = ({ goTo = -1 }) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleGoBack = () => {
-    navigate(goTo)
-  }
+    navigate(goTo);
+  };
 
   return (
-    <header className="fixed pt-1.5 top-5 left-0 flex flex-row-reverse justify-between items-center m-auto">
-      <span className="w-full flex justify-center py-4 mr-11 text-2xl">
-        
-      </span>
+    <header className="fixed top-5 left-0 flex flex-row-reverse justify-between items-center m-auto">
+      <span className="w-full flex justify-center py-4 mr-11 text-2xl"></span>
 
       <button
         className="text-lg px-1.5 py-1.5 border-2 border-neutral-200 rounded-xl focus:border-blue-700 ml-4 min-w-max flex-shrink-0"
@@ -22,7 +19,7 @@ const GoBack = ({ goTo = -1 }) => {
         <IoChevronBackOutline />
       </button>
     </header>
-  )
-}
+  );
+};
 
 export default GoBack;
