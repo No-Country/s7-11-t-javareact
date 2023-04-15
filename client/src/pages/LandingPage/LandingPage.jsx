b540e7ac46f38c89651d9a39b871086056922fc1
lucerocamila <lucerogonzalez1997@gmail.com>
2023-04-15 16:55:39 -0300

    style(LandingPage):update LandingPage view

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -6,6 +6,9 @@ import Logo from "../../assets/images/Logo.png";
 import Compra from "@/assets/images/Compra.png";
 import OrangeButton from "@/components/OrangeButton";
 import Listo from "@/assets/images/Listo.png";
+import angledUnderline from "@/assets/images/angledUnderline.png";
+import orangeSub from "@/assets/images/orangeSub.png";
+import NavBar from "@/layouts/NavBar";
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -18,10 +21,10 @@ const LandingPage = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen lg:h-full w-full lg:bg-contain bg-[url('../../assets/images/mainBg.png')]">
-      <div className="lg:gap-0 lg:grid lg:grid-cols-2 h-full bg-white bg-opacity-80 lg:py-5 lg:pl-6">
-        <div className="flex flex-col lg:flex-row lg:self-start lg:h-full items-center text-center p-4 lg:p-0">
-          {/*  div 1 *lg:bg-gray-200 */}
+    <div className="z-0 flex flex-col h-screen lg:h-full w-full lg:bg-contain bg-[url('../../assets/images/mainBg.png')]">
+      <div className="lg:gap-0 lg:grid lg:grid-cols-2 h-full bg-white bg-opacity-80  lg:pl-6">
+        <div className="flex flex-col lg:flex-row lg:self-start lg:h-full items-center text-center">
+          {/*  div 1 */}
           <div className="lg:hidden w-24 h-24 rounded-full shadow-md">
             <div className="w-full h-full rounded-full bg-white flex items-center justify-center shadow-lg border-4 border-gray-200">
               <img
@@ -31,35 +34,49 @@ const LandingPage = () => {
               />
             </div>
           </div>
-          <div className="flex flex-wrap py-4">
+          <div className="flex flex-wrap pt-2 sm:py-4">
             <img src={Compra} className="h-[5rem] hidden lg:block" />
             <img src={Listo} className="h-auto hidden lg:block" />
-            <p className="text-lg m-auto">
-              La comunidad de compradores eficientes
-            </p>
+            <div className="flex-col m-auto">
+              <p className="text-2xl tracking-2 font-damion">
+                La comunidad de compradores eficientes
+              </p>          <img className="w-[17rem] py-2 m-auto" src={angledUnderline} />
+
+            </div>
           </div>
         </div>
         <div className="flex-col items-center justify-center hidden  lg:block">
-          {/*  div 2 *lg:bg-gray-300 */}
+          {/*  div 2 */}
+          <h2 className="text-2xl text-center tracking-2 font-damion">
+            ¡Bienvenido!
+          </h2>
+          {/* <p className="text-2xl tracking-2 font-damion">
+          ¡Bienvenido! </p> */}
           <Login />
-          <div className="flex flex-col text-center gap-6 lg:m-auto pt-4 px-[16%] md:px-[30%]">
-            <p className="m-auto">Ahorra tiempo en tus compras y</p>
-            <h2 className="text-2xl font-bold">¡Regístrate hoy!</h2>
-            <OrangeButton text="Registrarse" onClick={controlRegisterClick} />
-          </div>
+          <img
+            className="hidden lg:block w-[26rem] lg:w-[36rem]"
+            src={angledUnderline}
+          />
         </div>
         <div className="flex flex-col items-center justify-center">
-          {/*  div 3 *lg:bg-gray-500 */}
-
+          {/*  div 3 */}
           <Carousel />
         </div>
-        <div className="px-4 w-full flex flex-col gap-4 text-center ">
-          {/*  div 4 *lg:bg-gray-400 */}
-          <div></div>
+
+        <div className="px-4 pb-4 w-full flex flex-col gap-4 text-center ">
+          {/*  div 4 */}
+          <div>
+            {" "}
+            <div className="flex flex-col text-center gap-4 sm:gap-6 lg:m-auto pt-4 sm:px-[14%] md:px-[30%]">
+              <p className="m-auto">Ahorra tiempo en tus compras y</p>
+              <h2 className="text-2xl font-bold">¡Regístrate hoy!</h2>
+              <OrangeButton text="Registrarse" onClick={controlRegisterClick} />
+            </div>
+          </div>
 
           <div className="flex flex-col lg:hidden">
             <p className="text-lg">
-              Si ya tienes una cuenta inicia sesión{" "}
+              Si ya tienes una cuenta inicia sesión
               <span>
                 <a
                   onClick={controlLoginClick}
