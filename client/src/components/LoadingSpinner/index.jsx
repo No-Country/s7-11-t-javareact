332d9a6de2e9199c2f90652529f3a1e3e2161b89
lucerocamila <lucerogonzalez1997@gmail.com>
2023-04-15 16:42:23 -0300

    feat(src):use LoadingSpinner, change wait time

---
+++ client/src/components/LoadingSpinner/index.jsx
@@ -0,0 +1,10 @@
+import smallLogo from "@/assets/images/smallLogo.png";
+const LoadingSpinner = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <img src={smallLogo} className="z-0 h-16 sm:h-22 w-auto m-auto animate-spinning-cart" />
+    </div>
+  );
+};
+
+export default LoadingSpinner;
