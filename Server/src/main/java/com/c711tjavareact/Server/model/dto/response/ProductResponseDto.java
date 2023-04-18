af61598b36f0d14b28ca9a681638c9bd72251be2
lokywolf2295 <matiasesek1995@gmail.com>
2023-04-18 19:57:16 -0300

    Update(Backend): remove requirementList atribute and import paths in ProductResponseDto class.
    Se borraron rutas de importaciones sin utilizar. Como así también el atributo requirementList. Además se corrigió el formato del código.

--- Server/src/main/java/com/c711tjavareact/Server/model/dto/response/ProductResponseDto.java
+++ Server/src/main/java/com/c711tjavareact/Server/model/dto/response/ProductResponseDto.java
@@ -1,26 +1,20 @@
 package com.c711tjavareact.Server.model.dto.response;
 
-import com.c711tjavareact.Server.model.entity.Requirement;
 import java.io.Serializable;
-import java.util.List;
 import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * A DTO for the {@link com.c711tjavareact.Server.model.entity.Product} entity
- */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductResponseDto implements Serializable {
-  private Long id;
-  @NotBlank(message = "Not Empty")
-  private String name;
-  private float price;
-  private int quantity;
-  private int discount;
-  private List<Requirement> requirementList;
+    private Long id;
+    @NotBlank(message = "Not Empty")
+    private String name;
+    private float price;
+    private int quantity;
+    private int discount;
 }
